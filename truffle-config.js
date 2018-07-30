a5b195f566c4faa76e253396d31e2519dc6f9551
Kosta Korenkov <kosta@whoot.me>
2018-07-30 17:32:48 +0300

    fix: revert in EthereumRuntime test

--- truffle-config.js
+++ truffle-config.js
@@ -26,6 +26,8 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*', // eslint-disable-line camelcase
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
     },
   },
 };
