f85fa7411deb61088fe39890f38e9b00bf7cf7d0
Kosta Korenkov <kosta@whoot.me>
2018-08-10 02:15:12 +0300

    add tests for stack-only opcodes

--- test/ethereumRuntime.js
+++ test/ethereumRuntime.js
@@ -1,13 +1,23 @@
-import { ADD, PUSH1 } from './helpers/constants';
+import fixtures from './fixtures';
+
+const OP = require('./helpers/constants');
+const { PUSH1 } = OP;
 
 const EthereumRuntime = artifacts.require('EthereumRuntime.sol');
+
+const opcodes = Object.keys(OP).reduce((s, k) => { s[OP[k]] = k; return s; }, {});
 
 const toNum = arr => arr.map(e => e.toNumber());
 
 contract('Runtime', function () {
+  let rt;
+  
+  before(async () => {
+    rt = await EthereumRuntime.new();
+  });
+
   it('should allow to add', async function () {
-    let rt = await EthereumRuntime.new();
-    const code = '0x' + PUSH1 + '03' + PUSH1 + '05' + ADD;
+    const code = '0x' + PUSH1 + '03' + PUSH1 + '05' + OP.ADD;
     const data = '0x';
     let rv = await rt.executeFlat(code, data);
     assert.equal(rv[2][0], 8);
@@ -15,8 +25,7 @@ contract('Runtime', function () {
 
   describe('executeAndStop', () => {
     it('should allow to stop at specified op-count', async function () {
-      let rt = await EthereumRuntime.new();
-      const code = '0x' + PUSH1 + '03' + PUSH1 + '05' + ADD;
+      const code = '0x' + PUSH1 + '03' + PUSH1 + '05' + OP.ADD;
       const data = '0x';
       let rv = await rt.executeAndStop(code, data, 2);
       assert.deepEqual(toNum(rv[2]), [3]);
@@ -30,24 +39,21 @@ contract('Runtime', function () {
   });
 
   describe('initAndExecute', () => {
-    it('should allow to continue from a specified op-count', async function () {
-      let rt = await EthereumRuntime.new();
-      const code = '0x' + PUSH1 + '03' + PUSH1 + '05' + ADD;
-      const data = '0x';
-      // execute steps 0 and 1 (PUSH 03) and stop.
-      let rv = await rt.executeAndStop(code, data, 2);
+    it('can continue from non-zero program counter', async () => {
+      const code = '0x' + PUSH1 + '03' + PUSH1 + '05' + OP.ADD;
+      let [,, stack, memory] = await rt.executeAndStop(code, '0x', 4);
+      [,, stack, memory] = await rt.initAndExecute(code, '0x', 4, stack, memory);
+      assert.deepEqual(toNum(stack), [8]);
+    });
 
-      // continue execution from step 2 passing in stack (rv[2]) amd memory (rv[3])
-      rv = await rt.initAndExecute(code, data, 2, rv[2], rv[3]);
-      // we expect script to complete
-      assert.deepEqual(toNum(rv[2]), [8]);
-
-      rv = await rt.executeAndStop(code, data, 4);
-      rv = await rt.initAndExecute(code, data, 4, rv[2], rv[3]);
-      assert.deepEqual(toNum(rv[2]), [8]);
-
-      rv = await rt.executeAndStop(code, data, 5);
-      assert.deepEqual(toNum(rv[2]), [8]);
+    fixtures.forEach(fixture => {
+      it(opcodes[fixture.opcode], async () => {
+        const code = `0x${fixture.opcode}`;
+        const [,, stack] = await rt.initAndExecute(code, '0x', 0, fixture.stack, '0x');
+        if (fixture.result.stack) {
+          assert.deepEqual(toNum(stack), fixture.result.stack);
+        }
+      });
     });
   });
 });
---
+++ test/fixtures.js
@@ -0,0 +1,76 @@
+const OP = require('./helpers/constants');
+
+const range = (from, to) => Array.from({ length: to - from + 1 }, (x, i) => i + from);
+
+const stack16 = range(1, 16);
+
+export default [
+  // 0x - arithmetic ops
+  { opcode: OP.ADD, stack: ['3', '5'], result: { stack: [8], gasUsed: 3 } },
+  { opcode: OP.MUL, stack: ['3', '5'], result: { stack: [15], gasUsed: 5 } },
+  { opcode: OP.SUB, stack: ['3', '5'], result: { stack: [2], gasUsed: 3 } },
+  { opcode: OP.DIV, stack: ['3', '6'], result: { stack: [2], gasUsed: 5 } },
+  { opcode: OP.SDIV, stack: ['3', '6'], result: { stack: [2], gasUsed: 5 } },
+  { opcode: OP.MOD, stack: ['3', '7'], result: { stack: [1], gasUsed: 5 } },
+  { opcode: OP.SMOD, stack: ['3', '8'], result: { stack: [2], gasUsed: 5 } },
+  { opcode: OP.ADDMOD, stack: ['5', '3', '5'], result: { stack: [3], gasUsed: 8 } },
+  { opcode: OP.MULMOD, stack: ['4', '3', '6'], result: { stack: [2], gasUsed: 8 } },
+  { opcode: OP.EXP, stack: ['3', '5'], result: { stack: [125], gasUsed: 10 } },
+  { opcode: OP.SIGNEXTEND, stack: ['3', '2'], result: { stack: [3], gasUsed: 5 } },
+
+  // 1x - Comparison & bitwise logic
+  { opcode: OP.LT, stack: ['5', '3', '2'], result: { stack: [5, 1], gasUsed: 3 } },
+  { opcode: OP.GT, stack: ['3', '2'], result: { stack: [0], gasUsed: 3 } },
+  { opcode: OP.SLT, stack: ['3', '2'], result: { stack: [1], gasUsed: 3 } },
+  { opcode: OP.SGT, stack: ['3', '2'], result: { stack: [0], gasUsed: 3 } },
+  { opcode: OP.EQ, stack: ['3', '2'], result: { stack: [0], gasUsed: 3 } },
+  { opcode: OP.ISZERO, stack: ['2'], result: { stack: [0], gasUsed: 3 } },
+  { opcode: OP.AND, stack: [0x11, 0x10], result: { stack: [parseInt('0x10', 16)], gasUsed: 3 } },
+  { opcode: OP.OR, stack: [0x101, 0x100], result: { stack: [parseInt('0x101', 16)], gasUsed: 3 } },
+  { opcode: OP.XOR, stack: [0x101, 0x100], result: { stack: [parseInt('0x1', 16)], gasUsed: 3 } },
+  {
+    opcode: OP.NOT,
+    stack: ['0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe'],
+    result: { stack: [1], gasUsed: 3 },
+  },
+  { opcode: OP.BYTE, stack: ['3', '2'], result: { stack: [0], gasUsed: 3 } },
+  // TODO: need to compile for Constantinople
+  // { opcode: OP.SHL, stack: [0x0001, 2], result: { stack: [parseInt('0x01', 16)] } },
+  // { opcode: OP.SHR, stack: [0x1000, 2], result: { stack: [parseInt('0x001', 16)] } },
+  // { opcode: OP.SAR, stack: [0x1000, 2], result: { stack: [1] } },
+  { opcode: OP.POP, stack: ['2', '3'], result: { stack: [2], gasUsed: 2 } },
+  // 8x Duplication
+  { opcode: OP.DUP1, stack: ['2'], result: { stack: [2, 2], gasUsed: 3 } },
+  { opcode: OP.DUP2, stack: stack16, result: { stack: [...stack16, 15], gasUsed: 3 } },
+  { opcode: OP.DUP3, stack: stack16, result: { stack: [...stack16, 14], gasUsed: 3 } },
+  { opcode: OP.DUP4, stack: stack16, result: { stack: [...stack16, 13], gasUsed: 3 } },
+  { opcode: OP.DUP5, stack: stack16, result: { stack: [...stack16, 12], gasUsed: 3 } },
+  { opcode: OP.DUP6, stack: stack16, result: { stack: [...stack16, 11], gasUsed: 3 } },
+  { opcode: OP.DUP7, stack: stack16, result: { stack: [...stack16, 10], gasUsed: 3 } },
+  { opcode: OP.DUP8, stack: stack16, result: { stack: [...stack16, 9], gasUsed: 3 } },
+  { opcode: OP.DUP9, stack: stack16, result: { stack: [...stack16, 8], gasUsed: 3 } },
+  { opcode: OP.DUP10, stack: stack16, result: { stack: [...stack16, 7], gasUsed: 3 } },
+  { opcode: OP.DUP11, stack: stack16, result: { stack: [...stack16, 6], gasUsed: 3 } },
+  { opcode: OP.DUP12, stack: stack16, result: { stack: [...stack16, 5], gasUsed: 3 } },
+  { opcode: OP.DUP13, stack: stack16, result: { stack: [...stack16, 4], gasUsed: 3 } },
+  { opcode: OP.DUP14, stack: stack16, result: { stack: [...stack16, 3], gasUsed: 3 } },
+  { opcode: OP.DUP15, stack: stack16, result: { stack: [...stack16, 2], gasUsed: 3 } },
+  { opcode: OP.DUP16, stack: stack16, result: { stack: [...stack16, 1], gasUsed: 3 } },
+  // 9x Exchange
+  { opcode: OP.SWAP1, stack: ['3', '2'], result: { stack: [2, 3], gasUsed: 3 } },
+  { opcode: OP.SWAP2, stack: range(1, 3), result: { stack: [3, ...range(2, 2), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP3, stack: range(1, 4), result: { stack: [4, ...range(2, 3), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP4, stack: range(1, 5), result: { stack: [5, ...range(2, 4), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP5, stack: range(1, 6), result: { stack: [6, ...range(2, 5), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP6, stack: range(1, 7), result: { stack: [7, ...range(2, 6), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP7, stack: range(1, 8), result: { stack: [8, ...range(2, 7), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP8, stack: range(1, 9), result: { stack: [9, ...range(2, 8), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP9, stack: range(1, 10), result: { stack: [10, ...range(2, 9), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP10, stack: range(1, 11), result: { stack: [11, ...range(2, 10), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP11, stack: range(1, 12), result: { stack: [12, ...range(2, 11), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP12, stack: range(1, 13), result: { stack: [13, ...range(2, 12), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP13, stack: range(1, 14), result: { stack: [14, ...range(2, 13), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP14, stack: range(1, 15), result: { stack: [15, ...range(2, 14), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP15, stack: range(1, 16), result: { stack: [16, ...range(2, 15), 1], gasUsed: 3 } },
+  { opcode: OP.SWAP16, stack: range(1, 17), result: { stack: [17, ...range(2, 16), 1], gasUsed: 3 } },
+];
