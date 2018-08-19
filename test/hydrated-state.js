98bd80952ac4b32dc68df093f9b3680aecfa98b8
Jaynti Kanani <jdkanani@gmail.com>
2018-08-19 19:39:41 +0530

    add testcase for multiplication

--- test/hydrated-state.js
+++ test/hydrated-state.js
@@ -4,6 +4,10 @@ import { getUnbalancedMerkleProof } from './helpers/unbalanced-merkle';
 const EthereumRuntime = artifacts.require('EthereumRuntime');
 const Hydrated = artifacts.require('Hydrated');
 
+function getSibling (val) {
+  return ethUtils.bufferToHex(ethUtils.keccak256(ethUtils.setLengthLeft(ethUtils.toBuffer(val), 32)));
+}
+
 contract('Hydrated', function () {
   let ethereumRuntime;
   let hydratedContract;
@@ -14,19 +18,48 @@ contract('Hydrated', function () {
   });
 
   it('should allow to prove ADD operation on-chain', async function () {
-    let beforeStack = [5, 8, 12];
-    let afterStack = [13, 12];
-
-    let beforeHash = getUnbalancedMerkleProof(beforeStack);
-    let afterHash = getUnbalancedMerkleProof(afterStack);
     let status = await hydratedContract.proveArithmeticOperation(
       '0x01', // ADD
-      beforeHash,
-      afterHash,
+      getUnbalancedMerkleProof([5, 8, 12]),
+      getUnbalancedMerkleProof([13, 12]),
       [5, 8],
-      ethUtils.bufferToHex(ethUtils.keccak256(ethUtils.setLengthLeft(ethUtils.toBuffer(12), 32))),
+      getSibling(12),
+      true
+    );
+    assert.equal(status, true);
+
+    // without sibling
+    status = await hydratedContract.proveArithmeticOperation(
+      '0x01', // ADD
+      getUnbalancedMerkleProof([5, 8]),
+      getUnbalancedMerkleProof([13]),
+      [5, 8],
+      '0x0',
       true
     );
     assert.equal(status, true);
   });
+
+  // it('should allow to prove MULT operation on-chain', async function () {
+  //   let status = await hydratedContract.proveArithmeticOperation(
+  //     '0x02', // ADD
+  //     getUnbalancedMerkleProof([5, 8, 89]),
+  //     getUnbalancedMerkleProof([40, 89]),
+  //     [5, 8],
+  //     getSibling(89),
+  //     true
+  //   );
+  //   assert.equal(status, true);
+
+  //   // without sibling
+  //   status = await hydratedContract.proveArithmeticOperation(
+  //     '0x02', // ADD
+  //     getUnbalancedMerkleProof([5, 8]),
+  //     getUnbalancedMerkleProof([40]),
+  //     [5, 8],
+  //     '0x0',
+  //     true
+  //   );
+  //   assert.equal(status, true);
+  // });
 });
