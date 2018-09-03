98eedfb327891780ee7c9a506ad99053ab89a09f
Jaynti Kanani <jdkanani@gmail.com>
2018-09-03 20:13:21 +0530

    more testcase for add operation

--- test/hydrated-state.js
+++ test/hydrated-state.js
@@ -35,6 +35,17 @@ contract('Hydrated', function () {
       getUnbalancedMerkleProof([13]),
       [5, 8],
       '0x0',
+      false
+    );
+    assert.equal(status, true);
+
+    // with sibling
+    status = await hydratedContract.proveArithmeticOperation(
+      '0x01', // ADD
+      getUnbalancedMerkleProof([5, 45, 78, 1234896, 45, 100033, 354372]),
+      getUnbalancedMerkleProof([50, 78, 1234896, 45, 100033, 354372]),
+      [5, 45],
+      getUnbalancedMerkleProof([78, 1234896, 45, 100033, 354372]),
       true
     );
     assert.equal(status, true);
@@ -42,7 +53,7 @@ contract('Hydrated', function () {
 
   it('should allow to prove MULT operation on-chain', async function () {
     let status = await hydratedContract.proveArithmeticOperation(
-      '0x02', // ADD
+      '0x02', // MUL
       getUnbalancedMerkleProof([5, 8, 89]),
       getUnbalancedMerkleProof([40, 89]),
       [5, 8],
@@ -53,12 +64,12 @@ contract('Hydrated', function () {
 
     // without sibling
     status = await hydratedContract.proveArithmeticOperation(
-      '0x02', // ADD
+      '0x02', // MUL
       getUnbalancedMerkleProof([5, 8]),
       getUnbalancedMerkleProof([40]),
       [5, 8],
       '0x0',
-      true
+      false
     );
     assert.equal(status, true);
   });
