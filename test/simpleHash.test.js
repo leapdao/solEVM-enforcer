75c3a257d9e576fbe208c2fd590f1d9bfb67c16b
peara <quang.vn@outlook.com>
2018-11-24 15:08:04 +0700

    Add test for calculate stack hash with hashed bottom elements

--- test/simpleHash.test.js
+++ test/simpleHash.test.js
@@ -9,14 +9,28 @@ contract('TestSimpleHash', function () {
   });
 
   it('correctly calculate hash of stack', async function () {
-    const arr = [3, 2, 1];
+    const arr = [1, 2, 3];
     let result = await simpleHash.testToHashStack(arr, 0);
     assert(result == getHash(arr, 0), 'Stack hash does not matched');
+  });
+
+  it('correctly calculate hash of stack with sibling', async function () {
+    const arr = [1, 2, 3, 4, 5];
+    let sibling = getHash(arr.slice(0, 2), 0); // Sibling is hash of [1, 2]
+    let result = await simpleHash.testToHashStack(arr.slice(2, 5), sibling);
+    assert(result == getHash(arr, 0), 'Stack hash with sibling does not matched');
   });
 
   it('correctly calculate hash of array', async function () {
     const arr = [1, 2, 3];
     let result = await simpleHash.testToHashArray(arr, 0);
     assert(result == getHash(arr, 0), 'Array hash does not matched');
+  });
+
+  it('correctly calculate hash of array with sibling', async function () {
+    const arr = [1, 2, 3, 4, 5];
+    let sibling = getHash(arr.slice(0, 2), 0); // Sibling is hash of [1, 2]
+    let result = await simpleHash.testToHashArray(arr.slice(2, 5), sibling);
+    assert(result == getHash(arr, 0), 'Array hash with sibling does not matched');
   });
 });
