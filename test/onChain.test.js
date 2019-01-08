29906da3a365a56421a857a6e673d439185d2c94
Vu Ngoc Quang <quang.vn@outlook.com>
2019-01-08 18:48:59 +0700

    allow EthereumRuntime to run a specific number of steps (#34)

--- test/onChain.test.js
+++ test/onChain.test.js
@@ -1,4 +1,4 @@
-import { toStr, getCode, deployContract } from './utils';
+import { toStr, getCodeWithStep, deployContract } from './utils';
 
 import onChainFixtures from './onChain.fixtures';
 import Runtime from './helpers/runtimeAdapter';
@@ -10,32 +10,31 @@ const EthereumRuntime = artifacts.require('EthereumRuntime.sol');
 
 contract('Runtime', function () {
   let rt;
-  
+
   before(async () => {
     rt = new Runtime(await deployContract(EthereumRuntime));
   });
 
   describe('execute - stop - execute one step - compare', () => {
     onChainFixtures.forEach(fixture => {
-      const { code, codeSize, pc, opcodeUnderTest } = getCode(fixture);
+      const { code, step, opcodeUnderTest } = getCodeWithStep(fixture);
       const callData = fixture.data || '0x';
 
       it(opcodeUnderTest, async () => {
         // 1. export the state right before the target opcode (this supposed to be off-chain)
-        const beforeState = await rt.executeAndStop(code, callData, [0, pc, BLOCK_GAS_LIMIT, BLOCK_GAS_LIMIT]);
+        const beforeState = await rt.executeAndStop(code, callData, [0, step, BLOCK_GAS_LIMIT, BLOCK_GAS_LIMIT]);
         // 2. export state right after the target opcode (this supposed to be off-chain)
-        const afterState = await rt.executeAndStop(code, callData, [0, pc + 1, BLOCK_GAS_LIMIT, BLOCK_GAS_LIMIT]);
-        
+        const afterState = await rt.executeAndStop(code, callData, [0, step + 1, BLOCK_GAS_LIMIT, BLOCK_GAS_LIMIT]);
+
         // 3. init with beforeState and execute just one step (target opcode) (this supposed to be on-chain)
-        const pcStart = pc;
-        // if pcStart is the last opcode then run till the end (pcEnd = 0)
-        const pcEnd = pcStart === codeSize - 1 ? 0 : pcStart + 1;
+        // console.log('Before', beforeState.stack);
         const onChainState = await rt.initAndExecute(
           code, callData,
-          [pcStart, pcEnd, BLOCK_GAS_LIMIT, beforeState.gasRemaining],
+          [beforeState.pc, 1, BLOCK_GAS_LIMIT, beforeState.gasRemaining],
           beforeState.stack, beforeState.mem, beforeState.accounts, beforeState.accountsCode,
           beforeState.logHash,
         );
+        // console.log('After', onChainState.stack);
 
         // 4. check that on-chain state is the same as off-chain
         assert.deepEqual(toStr(onChainState.stack), toStr(afterState.stack), 'Stack');
