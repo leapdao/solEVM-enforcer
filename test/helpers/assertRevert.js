cb5fa4f3af48b266ad2c3f0ccd00f490e5a57fc3
Vu Ngoc Quang <quang.vn@outlook.com>
2019-05-07 23:36:04 +0700

    Enforcer require dispute to have enough time to resolve

--- test/helpers/assertRevert.js
+++ test/helpers/assertRevert.js
@@ -1,4 +1,4 @@
-module.exports = async promise => {
+module.exports = async (promise, message) => {
   try {
     let tx = await promise;
     // geth doesn't fail immediately
@@ -7,6 +7,10 @@ module.exports = async promise => {
     // support for the unspecific error 'transaction failed' of geth
     const revertFound = error.message.search(/(revert|transaction failed)/) >= 0;
     assert(revertFound, `Expected "revert", got ${error} instead`);
+    if (message) {
+      const messageFound = error.message.search(message) >= 0;
+      assert(messageFound, `Expect ${message}, got ${error} instead`);
+    }
     return;
   }
   assert.fail('Expected revert not received');
