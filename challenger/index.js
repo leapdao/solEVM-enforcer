bd6e35a0f90f8f9dba01041befb2e14430d87b76
Alex Lunyov <isuntc@gmail.com>
2019-07-10 10:26:21 +0800

    Cache solutions for execution poker

--- challenger/index.js
+++ challenger/index.js
@@ -14,7 +14,31 @@ try {
   process.exit(1);
 }
 
-class MyExecutionPoker extends ExecutionPoker {}
+class MyExecutionPoker extends ExecutionPoker {
+  constructor(db, ...args) {
+    super(...args);
+    this.db = db;
+    this.restoreSolutions();
+    this.solutions = new Proxy(this.solutions, {
+      get(solutions, execId) {
+        return solutions[execId];
+      },
+      set(solutions, execId, result) {
+        solutions[execId] = result;
+
+        this.db.put('solutions', JSON.stringify(solutions));
+      }
+    })
+  }
+
+  restoreSolutions() {
+    this.db.get('solutions').then(json => {
+      this.solutions = JSON.parse(json); // ToDo: do proper deserialise here
+    }).catch(() => {
+      // do nothing
+    });
+  }
+}
 
 (async () => {
   const db = level('solEVM');
@@ -23,6 +47,15 @@ class MyExecutionPoker extends ExecutionPoker {}
   const enforcer = new ethers.Contract(cliArgs.enforcerAddr, Enforcer.abi, provider);
   const verifierAddr = await enforcer.verifier();
   const verifier = new ethers.Contract(verifierAddr, Verifier.abi, provider);
-  const executionPoker = new MyExecutionPoker(enforcer, verifier, wallet);
 
+  // ExecutionPoker will do the rest ¯\_(ツ)_/¯
+  new MyExecutionPoker(db, enforcer, verifier, wallet);
 })();
+
+function onException (e) {
+  console.error(e);
+  process.exit(1);
+}
+
+process.on('uncaughtException', onException);
+process.on('unhandledRejection', onException);
